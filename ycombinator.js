7d48cae1b755df3d666df6c839b53b67903b2db8
Torvaney <torvaney@protonmail.com>
2019-01-20 23:39:59 +0000

    Match single comments, too

--- ycombinator.js
+++ ycombinator.js
@@ -1,1 +1,1 @@
-document.body.innerHTML = document.body.innerHTML.replace(/(\d+)&nbsp;comments/g, "DEAD DOVE: Do Not Eat ($1)")
+document.body.innerHTML = document.body.innerHTML.replace(/(\d+)&nbsp;comments?/g, "DEAD DOVE: Do Not Eat ($1)")
